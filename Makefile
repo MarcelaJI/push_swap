817ef2a09ec57ca266e8fedf7fec6b4c41980a6c
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-07 11:25:12 +0200

    Adding project

---
+++ Makefile
@@ -0,0 +1,59 @@
+NAME	= push_swap
+CC		= cc
+CFLAGS	= -Wall -Wextra -Werror
+LIBFT_DIR	= Libft
+LIBFT_A		= $(LIBFT_DIR)/libft.a
+PRINTF		= ft_printf
+PRINF_A		= $(PRINTF)/libftprintf.a
+
+SRC		= src/instructions_1.c \
+			src/instructions_2.c \
+			src/instructions_3.c \
+			src/instructions_4.c \
+			src/utils.c \
+			src/push_swap.c \
+			src/quick_sort.c \
+			src/sort.c
+
+INCLUDE = -I include -I $(PRINTF)
+OBJS	= $(SRC:.c=.o)
+
+GREEN = \033[0;32m
+RED = \033[1;31m
+BLUE_UNDER = \033[1;34m
+YELLOW = \033[0;33m
+CYAN = \033[1;36m
+MAGENTA = \033[0;35m
+WHITE = \033[1;37m
+WHITE_RED_BG = \033[0;41;37m
+YELLOW_UNDER = \033[1;4;33m
+NC = \033[0m
+
+all: $(NAME)
+
+$(LIBFT_A):
+	@$(MAKE) -C $(LIBFT_DIR)
+
+$(PRINF_A):
+	@$(MAKE) -C $(PRINTF)
+
+$(NAME): $(OBJS) $(LIBFT_A) $(PRINF_A)
+	@echo "$(MAGENTA)Compiling $(NAME)$(NC)"
+	$(CC) $(CFLAGS) $(OBJS) $(INCLUDE) -L$(LIBFT_DIR) -L$(PRINTF) -lft -lftprintf -o $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@echo "$(RED)Removing .o files in all directories$(NC)"
+	rm -f $(OBJS)
+	@$(MAKE) clean -C $(LIBFT_DIR)
+	@$(MAKE) clean -C $(PRINTF)
+
+fclean: clean
+	@echo "$(RED)Removing .a files and executables$(NC)"
+	rm -f $(NAME)
+	@$(MAKE) fclean -C $(LIBFT_DIR)
+	@$(MAKE) fclean -C $(PRINTF)
+
+re: fclean all
---
+++ include/push_swap.h
@@ -0,0 +1,49 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include "../Libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_piles
+{
+	int	*a;
+	int	*b;
+	int	size_a;
+	int	size_b;
+}	t_piles;
+
+void	push_swap(char **av);
+int		push_swap_strlen(char **av);
+void	error_detected(int *pile);
+int		push_swap_atoi(char *str, int *pile);
+void	check_doubles(int *pile, int size);
+//Instructions
+void	swap_a(t_piles *pile, int bonus);
+void	swap_b(t_piles *pile, int bonus);
+void	swap_s(t_piles *pile, int bonus);
+void	rotate_a(t_piles *pile, int bonus);
+void	rotate_b(t_piles *pile, int bonus);
+void	rotate_r(t_piles *pile, int bonus);
+void	reverse_rotate_a(t_piles *pile, int bonus);
+void	reverse_rotate_b(t_piles *pile, int bonus);
+void	reverse_rotate_r(t_piles *pile, int bonus);
+void	push_b(t_piles *pile, int bonus);
+void	push_a(t_piles *pile, int bonus);
+//Sort
+int		sort(t_piles *pile, int size);
+int		check_sorted(int *pile, int size, int order);
+void	three_numbers_case_pile_a(t_piles *three);
+void	temporary_sort(int *pile_temporaire, int size);
+//Quicksort
+int		quicksort_pile_a(t_piles *pile, int len, int count_r);
+int		quicksort_pile_b(t_piles *pile, int len, int count_r);
+void	quicksort_three_pile_a_and_b(t_piles *pile, int len);
+int		sort_three_b(t_piles *pile, int len);
+int		ft_push(t_piles *pile, int len, int pb);
+int		mediane_of_numbers(int *pivot, int *pile, int size);
+
+#endif
