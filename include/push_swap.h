3fd459d4aca51803711a108614cec213a6375444
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-12 09:36:39 +0200

    Adding colors

--- include/push_swap.h
+++ include/push_swap.h
@@ -9,6 +9,15 @@
 # include <stddef.h> 
 # include <unistd.h>
 
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+# define RESET "\033[0m"
+
 typedef struct s_stack
 {
 	int		*content; // Valor del nodo
