af62862987ed2b241334d4b3a93b30b443a4654d
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-12 08:45:55 +0200

    Adding main

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,15 +1,23 @@
 #include "../include/push_swap.h"
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	t_stack	*stack_a;
+	t_stack		*stack_a;
 
 	stack_a = NULL;
-	if (argc > 1)
+	if (argc < 2)
+		return (0);
+	if (!parse_data(argc, &argv))
 	{
-		stack_a = parse_data(argc, argv);
-		if (!stack_a)
-			return (-1);
-		if 
+		ft_putstr_fd("Error\n", 2);
+		return (-1);
 	}
+	if (is_sorted(stack_a))
+	{
+		free_stack(stack_a);
+		return (0);
+	}
+	sorted(stack_a);
+	free_stack(stack_a);
+	return (0);
 }
