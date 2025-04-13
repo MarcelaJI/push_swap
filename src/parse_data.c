c947133580075482b116450badef439fe729dcf8
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-13 14:55:01 +0200

    Adding changes

--- src/parse_data.c
+++ src/parse_data.c
@@ -1,21 +1,2 @@
 #include "../include/push_swap.h"
 
-t_stack     *parse_data(int argc, char **argv)
-{
-    char    **arr;
-    t_stack     *stack;
-
-    arr = NULL;
-    stack = NULL;
-    if (argc < 2)
-    {   
-        ft_printf("Insufficient number of arguments");
-        return (NULL);
-    }
-    arr = get_arr(argc, argv);
-    if (!arr)
-        return (NULL);
-    stack = validate_arr(arr);
-    free_str_arr(&arr);
-    return (stack);
-}
