e0cd9f2963874820d2af563595ca70c87b005b21
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-22 10:52:43 +0200

    Adding test main

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,6 +3,7 @@
 int		main(int argc, char **argv)
 {
 	t_stack	*stack_a;
+	t_state state;
 
 	if (argc < 2)
 		return (0);
@@ -12,13 +13,22 @@ int		main(int argc, char **argv)
 		ft_printf("Error en el stack_a\n");
 		return (EXIT_FAILURE);
 	}
-	if (ft_is_sorted(stack_a))
-	{
-		free_stack(&stack_a);
-		return (EXIT_SUCCESS);
-	}
+	// if (ft_is_sorted(stack_a))
+	// {
+	// 	free_stack(&stack_a);
+	// 	return (EXIT_SUCCESS);
+	// }
 	set_index(stack_a);
-	print_stack(stack_a);
+	state.stack_a = stack_a;
+	state.stack_b = NULL;
+	state.size_a = argc - 1;
+	state.size_b = 0;
+
+	ft_printf("Antest de sa:\n");
+	print_stack(state.stack_a);
+	ft_sa(&state);
+	ft_printf("Después de sa\n");
+	print_stack(state.stack_a);
 	free_stack(&stack_a);
 	return (EXIT_SUCCESS);
 }
