ce691ecffbfac32d2c97d9ef394f398bbeddfc98
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-26 09:13:19 +0200

    Adding changes in main

--- src/push_swap.c
+++ src/push_swap.c
@@ -2,18 +2,21 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack	*stack;
+	t_stack	*stack_a;
 
 	if (argc < 2)
 		return (0);
-	stack = parse_data(argv);
-	if (!stack)
+	if (validate_and_convert(argv) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	if (!ft_is_sorted(stack))
-		sort(stack);
-	free_stack(&stack);
+	stack_a = parse_data(argv);
+	if (!stack_a)
+		return (EXIT_FAILURE);
+	if (!ft_is_sorted(stack_a))
+		sort(stack_a);
+	free_stack(&stack_a);
 	return (0);
 }
+
 
 
 
